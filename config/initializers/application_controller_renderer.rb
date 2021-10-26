c008975ddddd2e5a5c6f6653b047d0b1ed1c313f
A-Maruyama-dmm <com.mac.dm@gmail.com>
2021-10-26 08:12:27 +0000

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'd124683a9f01e0410bc5817f2f2d6fd6547d5b2008b816bb2b65b2a806d3011043c1a47f78e1c7c555306f0763d9ac3ec0e1eefdf52d80e073626e7764e8ba26'
